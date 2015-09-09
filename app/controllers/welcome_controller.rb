c575f333d4b0d283c746467da2b1be8e9bc7bf3a
Kevin Smith <ksmithbaylor@gmail.com>
2015-09-09 09:08:39 -0400

    More changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @homeland = 'Puerto Rico'
     @countries = ['Chile', 'Bolivia', 'Croatia']
+    @images = ['chile.jpg', 'bolivia.jpg' ,'croatia.jpg']
   end
 
   def about
